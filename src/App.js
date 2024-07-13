a8a76300bcc3aea9607ef0d2ae74c451e47e2fbe
Yashasvi Gupta <yashcode17@gmail.com>
2024-07-13 19:27:30 +0530

    Adding Dark Mode in Componenets(TextForm and navBar)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/About';
+import { useState } from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title="YashCode" aboutText="About"/>
+      <Navbar title="YashCode" aboutText="About" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        {/* <TextForm heading="Enter The Text Here"/> */}
-        <About/>
+        <TextForm heading="Enter The Text Here" mode={mode}/>
+        {/* <About/> */}
       </div>
       
     </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -19,6 +19,11 @@ export default function TextForm(props) {
         let newText = "";
         setText(newText);
     }
+    const handleRealityClick = ()=>{
+        // console.log("Uppercase was clicked");
+        let newText = "";
+        setText("Sach Sach Bola Kro!");
+    }
     const handleOnChange = (event)=>{
         // console.log("On Change");
         setText(event.target.value);
@@ -26,20 +31,21 @@ export default function TextForm(props) {
     
     return (
         <>
-        <div>
+        <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} id="myBox" rows="8" onChange={handleOnChange}/>
+                <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8" onChange={handleOnChange}/>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>CONVERT TO UPPER CASE</button>
             <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to lower case</button>
             <button className="btn btn-primary" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-3" onClick={handleRealityClick}>Reality Text</button>
         </div>
-        <div className="container">
-            <h1>Your Text Summary:</h1>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h1 className='my-3'>Your Text Summary:</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:'Kuch to likho'}</p>
         </div>
         </>
     )
